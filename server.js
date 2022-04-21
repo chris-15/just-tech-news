e2e28c72e113141f6dc104664b83d10a0335b9b6
chris-15 <chrismac@Chriss-MacBook-Pro.local>
2022-04-21 19:27:08 -0400

    adding partials and helpers

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+
 const session = require('express-session');
+
+
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -21,6 +23,9 @@ const sess = {
 };
 
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
